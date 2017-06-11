26d5e9412955df050f22d5cfbdd00d653a2d936d
Manikumar1998 <manikumarperla18@gmail.com>
2017-06-11 20:40:06 +0530

    Add from_biopython(), _modified_features in Record

--- secmet/record.py
+++ secmet/record.py
@@ -4,8 +4,6 @@
 
 from Bio import SeqIO
 
-#pylint: disable=W0312,C0111
-
 class Feature(object):
 	"""A Feature super class that expands to different subclasses"""
 	def __init__(self, feature):
@@ -17,7 +15,9 @@ class Feature(object):
 		self.location = self.feature.location
 		self.type = self.feature.type
 
+	@property
 	def to_biopython(self):
+		"""Returns a Bio.SeqFeature.SeqFeature object of same feature"""
 		return [self.feature]
 
 
@@ -31,10 +31,6 @@ class GenericFeature(Feature):
 		"""
 		if feature != None:
 			Feature.__init__(self, feature)
-
-	def to_biopython(self):
-		"""Returns Bio.SeqFeature.SeqFeature object of the same feature"""
-		return [self.feature]
 
 	def add_qualifier(self, category, info):
 		"""Adds a qualifier to qualifiers dictionary"""
@@ -79,13 +75,16 @@ class CDSFeature(Feature):
 		else:
 			self.gene = None
 
+	@property
 	def get_id(self):
 		"""Returns the id of the CDSFeature"""
 		return self.gene
 
-	def to_biopython(self):
-		"""Returns a Bio.SeqFeature.SeqFeature object of the same feature"""
-		return [self.feature]
+	@property
+	def get_cluster(self):
+		"""Returns a ClusterFeature"""
+		#TO-DO: Should return the corresponding ClusterFeature
+		return
 
 
 
@@ -99,88 +98,139 @@ class ClusterFeature(Feature):
 			Feature.__init__(self, feature)
 		if 'cutoff' in self._qualifiers:
 			self.cutoff = self._qualifiers['cutoff']
+		else:
+			self.cutoff = None
 		if 'extension' in self._qualifiers:
 			self.extension = self._qualifiers['extension']
+		else:
+			self.extension = None
 		if 'contig_edge' in self._qualifiers:
 			self.contig_edge = self._qualifiers['contig_edge']
+		else:
+			self.contig_edge = None
 		if 'detection' in self._qualifiers:
 			self.detection = self._qualifiers['note'][1]
+		else:
+			self.detection = None
 
-	def to_biopython(self):
-		"""Returns a Bio.SeqFeature.SeqFeature object of the same feature"""
-		return [self.feature]
-
+	@property
 	def get_products(self):
 		"""Returns product qualifier from ClusterFeature object"""
 		return self._qualifiers['product']
 
+	@property
 	def get_cluster_number(self):
 		"""Returns the clusternumber of the cluster"""
 		return int(self._qualifiers['note'][0].split()[2])
 
+	@property
+	def get_CDSs(self):
+		#TO-DO: Should return a list of CDSFeatures
+		return
+
+	def add_product(self, product):
+		"""Adds a product qualifier to the ClusterFeature object"""
+		self._qualifiers['product'].append(product)
+
 
 class Record(object):
-    """A record containing secondary metabolite clusters"""
+	"""A record containing secondary metabolite clusters"""
 
-    def __init__(self, seq_record=None):
-        """Initialise a secondary metabolite record
+	def __init__(self, seq_record=None):
+		"""Initialise a secondary metabolite record
 
-        :param seq_record:  :class:`Bio.SeqRecord.SeqRecord` to read
-        :type seq_record:   :class:`Bio.SeqRecord.SeqRecord`
-        """
-        self._record = seq_record
-
-
-    @classmethod
-    def from_genbank(cls, filename):
-        """Initialise a record from a GenBank file
-
-        :param string filename:    file name of the GenBank file to read
-        """
-        seq_record = SeqIO.read(filename, 'genbank')
-        rec = cls(seq_record=seq_record)
-        return rec
+		:param seq_record:  :class:`Bio.SeqRecord.SeqRecord` to read
+		:type seq_record:   :class:`Bio.SeqRecord.SeqRecord`
+		"""
+		self._record = seq_record
+		self._modified_features = [] #A list containing instances of Feature or its subclasses
 
 
-    @property
-    def id(self):
-        """Pass through to seq_record object if available"""
-        if self._record is not None:
-            return self._record.id
-        else:
-            return "NO_ID_ASSIGNED"
+	@classmethod
+	def from_genbank(cls, filename):
+	    """Initialise a record from a GenBank file
+
+	    :param string filename:    file name of the GenBank file to read
+	    """
+	    seq_record = SeqIO.read(filename, 'genbank')
+	    rec = cls(seq_record=seq_record)
+	    return rec
 
 
-    @property
-    def seq(self):
-        """Pass through to seq_record object if available"""
-        if self._record is not None:
-            return self._record.seq
-        else:
-            return None
+	@property
+	def id(self):
+	    """Pass through to seq_record object if available"""
+	    if self._record is not None:
+	        return self._record.id
+	    else:
+	        return "NO_ID_ASSIGNED"
 
 
-    @property
-    def annotations(self):
-        """Pass through to seq_record object if available"""
-        if self._record is not None:
-            return self._record.annotations
-        else:
-            return {}
+	@property
+	def seq(self):
+	    """Pass through to seq_record object if available"""
+	    if self._record is not None:
+	        return self._record.seq
+	    else:
+	        return None
 
-    @property
-    def description(self):
-        """Pass through to seq_record object if available"""
-        if self._record is not None:
-            return self._record.description
-        else:
-            return ""
 
-    @property
-    def clusters(self):
-        """A list of secondary metabolite clusters present in the record"""
-        if self._record is None:
-            return []
+	@property
+	def annotations(self):
+	    """Pass through to seq_record object if available"""
+	    if self._record is not None:
+	        return self._record.annotations
+	    else:
+	        return {}
 
-        clusters = [i for i in self._record.features if i.type == 'cluster']
-        return clusters
+	@property
+	def description(self):
+	    """Pass through to seq_record object if available"""
+	    if self._record is not None:
+	        return self._record.description
+	    else:
+	        return ""
+
+	@property
+	def get_clusters(self):
+		"""A list of secondary metabolite clusters present in the record"""
+		if self._record is None:
+		    return []
+
+		clusters = [i for i in self._modified_features if i.type == 'cluster']
+		return clusters
+
+	@property
+	def get_CDSs(self):
+		"""A list of secondary metabolite clusters present in the record"""
+		if self._record is None:
+		    return []
+
+		clusters = [i for i in self._modified_features if i.type == 'CDS']
+		return clusters
+
+	@property
+	def to_biopython(self):
+		"""Returns a Bio.SeqRecord instance"""
+		return self._record
+
+	def get_cluster_number(self, clusterfeature):
+		"""Returns cluster number of a cluster feature
+			param ClusterFeature clusterfeature : A instance of ClusterFeature class
+		"""
+		return clusterfeature.get_cluster_number
+
+	def from_biopython(self):
+		"""Modifies _modified_features list with new Feature instances"""
+		features = self._record.features
+		for feature in features:
+			if feature.type == 'CDS':
+				feature = CDSFeature(feature)
+				self._modified_features.append(feature)
+			elif feature.type == 'cluster':
+				feature = ClusterFeature(feature)
+				self._modified_features.append(feature)
+			else:
+				feature = GenericFeature(feature)
+				self._modified_features.append(feature)
+		return self
