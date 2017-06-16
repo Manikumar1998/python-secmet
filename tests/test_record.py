100db1772b6969ebdfca7fc7d5926a15c4661a89
Manikumar1998 <manikumarperla18@gmail.com>
2017-06-16 18:23:55 +0530

    Update test_record.py

--- tests/test_record.py
+++ tests/test_record.py
@@ -1,25 +1,20 @@
 from os import path
 import Bio
 from Bio import SeqIO
+from Bio.SeqFeature import FeatureLocation
 from secmet.record import Record
 from secmet.record import GenericFeature, ClusterFeature, CDSFeature
 
 
-def get_testfile(filename):
-    return path.join(path.dirname(__file__), 'data', filename)
+filename = 'nisin.gbk'
 
-
-def test_init_empty():
-    rec = Record()
-    assert isinstance(rec, Record)
-    assert rec.id == "NO_ID_ASSIGNED"
-    assert rec.seq is None
-    assert rec.annotations == {}
-    assert rec.description == ""
-    assert rec.clusters == []
+def get_testfile():
+    """File path for testing"""
+    return path.join(path.dirname(__file__), 'tests/data', filename)
 
 def test_from_file():
-    testfile = get_testfile('nisin.gbk')
+    """Test file operations in Record"""
+    testfile = get_testfile()
     bp_rec = SeqIO.read(testfile, 'genbank')
     rec = Record.from_file(testfile, 'genbank')
     assert isinstance(rec, Record)
@@ -33,31 +28,64 @@ def test_from_file():
     assert rec.description == bp_rec.description
 
 def test_from_biopython():
-	testfile = get_testfile('nisin.gbk')
-	rec = Record.from_file(testfile, 'genbank')
-	assert isinstance(rec.from_biopython(), Record)
+    """Test from_biopython() in Record"""
+    testfile = get_testfile()
+    rec = Record.from_file(testfile, 'genbank')
+    assert isinstance(rec.from_biopython(rec._record), Record)
 
 def test_to_biopython():
-	testfile = get_testfile('nisin.gbk')
-	rec = Record.from_file(testfile, 'genbank')
-	assert isinstance(rec.to_biopython, Bio.SeqRecord.SeqRecord)
+    """Test to_biopython() in Record"""
+    testfile = get_testfile()
+    rec = Record.from_file(testfile, 'genbank')
+    assert isinstance(rec.to_biopython(), Bio.SeqRecord.SeqRecord)
 
 def test_get_clusters():
-    testfile = get_testfile('nisin.gbk')
+    """Test get_clusters() in Record"""
+    testfile = get_testfile()
     rec = Record.from_file(testfile, 'genbank')
     bp_rec = SeqIO.read(testfile, 'genbank')
-    clusters = [i for i in bp_rec.features if i.type == 'cluster']
-	#Should call from_biopython() to access features
-    rec.from_biopython()
-    assert len(rec.get_clusters) == len(clusters)
-    assert isinstance(rec.get_clusters[0], ClusterFeature)
-    
+    bp_clusters = [i for i in bp_rec.features if i.type == 'cluster']
+    mod_clusters = rec.get_clusters()
+    assert len(mod_clusters) == len(bp_clusters)
+    for cluster in mod_clusters:
+        assert isinstance(cluster, ClusterFeature)
+
 def test_get_CDSs():
-    testfile = get_testfile('nisin.gbk')
+    """Test get_CDSs() in Record"""
+    testfile = get_testfile()
     rec = Record.from_file(testfile, 'genbank')
     bp_rec = SeqIO.read(testfile, 'genbank')
-    CDSs = [i for i in bp_rec.features if i.type == 'CDS']
-	#Should call from_biopython() to access features
-    rec.from_biopython()
-    assert len(rec.get_CDSs) == len(CDSs)
-    assert isinstance(rec.get_CDSs[0], CDSFeature)
+    bp_CDSs = [i for i in bp_rec.features if i.type == 'CDS']
+    mod_CDSs = rec.get_CDSs()
+    assert len(mod_CDSs) == len(bp_CDSs)
+    for cds in mod_CDSs:
+        assert isinstance(cds, CDSFeature)
+
+def test_get_cluster_number():
+    """Test get_cluster_number() in Record"""
+    testfile = get_testfile()
+    rec = Record.from_file(testfile, 'genbank')
+    cluster = rec.get_clusters()[0]
+    assert rec.get_cluster_number(cluster) == 1
+
+def test_add_feature():
+    """Test add_feature() in Record"""
+    testfile = get_testfile()
+    rec = Record.from_file(testfile, 'genbank')
+    no_of_clusters = len(rec.get_clusters())
+    no_of_cdss = len(rec.get_CDSs())
+    no_of_generics = len(rec._modified_Generic)
+    new_cluster = ClusterFeature()
+
+    #ClusterFeature should have valid location for adding
+    new_cluster.location = FeatureLocation(15100, 15200)
+    new_cds = CDSFeature()
+    new_generic = GenericFeature()
+    rec.add_feature(new_cluster)
+    rec.add_feature(new_cds)
+    rec.add_feature(new_generic)
+    assert no_of_clusters+1 == len(rec.get_clusters())
+    assert no_of_cdss+1 == len(rec.get_CDSs())
+    assert no_of_generics+1 == len(rec._modified_Generic)
+    assert rec.get_clusters()[0].get_cluster_number() == 1
+    assert new_cluster.get_cluster_number() == 2
