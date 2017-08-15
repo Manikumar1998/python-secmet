b7829bac5eea4bd6b99d74fea4b86c8346475e03
Manikumar1998 <manikumarperla18@gmail.com>
2017-08-15 07:20:58 +0530

    Add uniitest module in test_cluster.py

--- tests/test_cluster.py
+++ tests/test_cluster.py
@@ -1,47 +1,22 @@
 from os import path
+import unittest
 from Bio import SeqIO
 from Bio.SeqFeature import FeatureLocation
-from secmet.record import Record
-from secmet.record import ClusterFeature
+from secmet.record import Record, ClusterFeature
 
 filename = 'nisin.gbk'
 filetype = 'genbank'
 
-def get_testfile():
-    """File path for testing"""
-    return path.join(path.dirname(__file__), 'data', filename)
+class TestClusterFeature(unittest.TestCase):
+    def get_testfile(self):
+        """File path for testing"""
+        return path.join(path.dirname(__file__), 'data', filename)
 
-def test_add_new_cluster():
-    """Test for adding a new cluster to record"""
-    testfile = get_testfile()
-    rec = Record.from_file(testfile)
-    new_cluster = ClusterFeature()
-    new_cluster.location = FeatureLocation(100, 500)
-    try:
-        new_cluster.cutoff = 300
-    except:
-        raise ValueError('Error assigning cutoff value')
-    try:
-        new_cluster.extension = 300
-    except:
-        raise ValueError('Error assiging extension value')
-    new_cluster.contig_edge = True
-    new_cluster.detection = 'Detection rules...'
-    new_cluster.add_product('product_info')
-    no_clusters_initial = len(rec.get_clusters())
-    rec.add_feature(new_cluster)
-    no_clusters_final = len(rec.get_clusters())
-    assert no_clusters_initial+1 == no_clusters_final
-    return new_cluster
-
-def test_add_existing_cluster():
-    """Test for accessing the existing cluster from record"""
-    testfile = get_testfile()
-    rec = Record.from_file(testfile)
-    if len(rec.get_clusters()) >= 1:
-        new_cluster = rec.get_clusters()[0]
-        assert isinstance(new_cluster, ClusterFeature)
-        new_cluster.location = FeatureLocation(100, 500)
+    def test_add_new_cluster(self):
+        """Test for adding a new cluster to record"""
+        testfile = self.get_testfile()
+        rec = Record.from_file(testfile)
+        new_cluster = ClusterFeature(FeatureLocation(100, 500))
         try:
             new_cluster.cutoff = 300
         except:
@@ -50,26 +25,51 @@ def test_add_existing_cluster():
             new_cluster.extension = 300
         except:
             raise ValueError('Error assiging extension value')
+        new_cluster.contig_edge = True
+        new_cluster.detection = 'Detection rules...'
+        new_cluster.add_product('product_info')
+        no_clusters_initial = len(rec.get_clusters())
         rec.add_feature(new_cluster)
+        no_clusters_final = len(rec.get_clusters())
+        assert no_clusters_initial+1 == no_clusters_final
         return new_cluster
 
-def write_to_file():
-    """Write data from test_add_new_cluster()"""
-    testfile = get_testfile()
-    rec = Record.from_file(testfile)
-    new_cluster_feature = test_add_new_cluster()
-    rec.add_feature(new_cluster_feature)
-    record_1 = rec.to_biopython()
-    with open('test_new_'+filename, 'w') as handle:
-        SeqIO.write([record_1], handle, filetype)
+    def test_add_existing_cluster(self):
+        """Test for accessing the existing cluster from record"""
+        testfile = self.get_testfile()
+        rec = Record.from_file(testfile)
+        if len(rec.get_clusters()) >= 1:
+            new_cluster = rec.get_clusters()[0]
+            assert isinstance(new_cluster, ClusterFeature)
+            new_cluster.location = FeatureLocation(100, 500)
+            try:
+                new_cluster.cutoff = 300
+            except:
+                raise ValueError('Error assigning cutoff value')
+            try:
+                new_cluster.extension = 300
+            except:
+                raise ValueError('Error assiging extension value')
+            rec.add_feature(new_cluster)
+            return new_cluster
 
-    #Write data from test_add_existing_cluster()
-    rec = Record.from_file(testfile)
-    try:
-        new_cluster_feature = test_add_existing_cluster()
+    def test_write_to_file(self):
+        """Write data from test_add_new_cluster()"""
+        testfile = self.get_testfile()
+        rec = Record.from_file(testfile)
+        new_cluster_feature = self.test_add_new_cluster()
         rec.add_feature(new_cluster_feature)
-    except TypeError:   #To return if no clusters are already present in the file
-        return
-    record_2 = rec.to_biopython()
-    with open('test_existing_'+filename, 'w') as handle:
-        SeqIO.write([record_2], handle, filetype)
+        record_1 = rec.to_biopython()
+        with open('test_new_'+filename, 'w') as handle:
+            SeqIO.write([record_1], handle, filetype)
+
+        #Write data from test_add_existing_cluster()
+        rec = Record.from_file(testfile)
+        try:
+            new_cluster_feature = self.test_add_existing_cluster()
+            rec.add_feature(new_cluster_feature)
+        except TypeError:   #To return if no clusters are already present in the file
+            return
+        record_2 = rec.to_biopython()
+        with open('test_existing_'+filename, 'w') as handle:
+            SeqIO.write([record_2], handle, filetype)
